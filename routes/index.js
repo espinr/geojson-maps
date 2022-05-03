6f1640bf943300eebce7ea97164ecd37f2b17c35
Martin Alvarez <martin@espinr.es>
2022-05-03 10:39:41 +0200

    Keeps support of geojson

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 /* GET home page. 
 * Query string params:
 *  - 'icon' with the color of the icon: (red, blue, teal, brown, green, yellow, pink) (example: &icon=pale)
+*  - 'geojson' with the geojson string to show on the map (if this is found, the rest of the parameters are omitted)
 *  - 'url' with the url of the database to extract the features
 *  - 'locale' the lang tag of the content to extract the names of the features
 */
@@ -20,42 +21,59 @@ router.get('/', function(req, res, next) {
     color = 'red' // by default
   }
 
-  // It could receive the URL to the configuration file
-    if (req.query.url && req.query.locale) {
-      fetch(req.query.url)
-      .then(res => res.json())
-      .then(json => {
-        try {
-          const items = json.content[req.query.locale].pois
-          for (let i = 0; i < items.length; i++) {
-            const element = items[i];
-            geojson.features.push({
-              type: "Feature",
-              properties: {
-                popupContent: element.name
-              },
-              geometry: {
-                type: "Point",
-                coordinates: [ parseFloat(element.lon), parseFloat(element.lat) ]
-              }
-            })
-          }
-          if(!gjv.valid(geojson)) {
-            // reset the geojson
-            geojson = { type : "FeatureCollection", features: []}
-            console.error('Not a valid GeoJSON')
-            const trace = gjv.isFeature(geojson, true)
-            console.error(trace)
-          } 
-          res.render('index', { title: 'Map Generator', geojson: geojson, colorIcon: color });
-        } catch(e) {
-          console.error(`I cannot parse the JSON file (request query: ${req.query})`)
-          console.error(e)
-          geojson = { type : "FeatureCollection", features: []}
-          res.render('index', { title: 'Map Generator', geojson: geojson, colorIcon: color });
-        }
-      })
+  // First, checks the geojson parameter (top priority)
+  if (req.query.geojson) {
+    try {
+      geojson = JSON.parse(req.query.geojson)
+      if(!gjv.valid(geojson)) {
+        geojson = { type : "FeatureCollection", features: []}
+        console.error('Not a valid GeoJSON')
+        const trace = gjv.isFeature(req.query.geojson, true)
+        console.error(trace)
+      } 
+    } catch (e) {
+      geojson = { type : "FeatureCollection", features: []}
+      console.error('Not a valid GeoJSON ' + e)
     }
+    res.render('index', { title: 'Map Generator', geojson: geojson, colorIcon: color });
+  }
+
+  // It could receive the URL to the configuration file
+  if (req.query.url && req.query.locale) {
+    fetch(req.query.url)
+    .then(res => res.json())
+    .then(json => {
+      try {
+        const items = json.content[req.query.locale].pois
+        for (let i = 0; i < items.length; i++) {
+          const element = items[i];
+          geojson.features.push({
+            type: "Feature",
+            properties: {
+              popupContent: element.name
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [ parseFloat(element.lon), parseFloat(element.lat) ]
+            }
+          })
+        }
+        if(!gjv.valid(geojson)) {
+          // reset the geojson
+          geojson = { type : "FeatureCollection", features: []}
+          console.error('Not a valid GeoJSON')
+          const trace = gjv.isFeature(geojson, true)
+          console.error(trace)
+        } 
+        res.render('index', { title: 'Map Generator', geojson: geojson, colorIcon: color });
+      } catch(e) {
+        console.error(`I cannot parse the JSON file (request query: ${req.query})`)
+        console.error(e)
+        geojson = { type : "FeatureCollection", features: []}
+        res.render('index', { title: 'Map Generator', geojson: geojson, colorIcon: color });
+      }
+    })
+  }
 });
 
 module.exports = router;
